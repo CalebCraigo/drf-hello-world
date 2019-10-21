619dfbee3eb15fe55de5e47e4ae4554f9f89d6c7
CalebCraigo <CalebDOS1@yahoo.com>
2019-10-21 13:58:11 -0400

    adds build directory

---
+++ frontend/static/build/precache-manifest.7c6102aff296b47c5f968d8c007ed168.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db3acd1a9c8284303635b0406e9107c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4467cfca6f9ca41a563",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0c5f556b58ee8825dca6",
+    "url": "/static/js/2.0760e9c0.chunk.js"
+  },
+  {
+    "revision": "c4467cfca6f9ca41a563",
+    "url": "/static/js/main.180b8048.chunk.js"
+  },
+  {
+    "revision": "71dcedd2f2bbe7dcbd91",
+    "url": "/static/js/runtime-main.107b9db7.js"
+  }
+]);
